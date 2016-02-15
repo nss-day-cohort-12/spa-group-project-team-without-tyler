f26bc20e144cff9ea6c822ce3e75541461b50b4b
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-02-15 09:43:57 -0600

    first IIFE returns messages

--- chatty.js
+++ chatty.js
@@ -1,16 +1,16 @@
 var Chatty = function () {
 
 	return {
-	    // make request for contents of messages.json
+	   // make request for contents of messages.json
 	  loadMessages: function () {
 	    var messageRequest = new XMLHttpRequest;
 	    messageRequest.open("GET", "messages.json");
 	    messageRequest.send();
-	  // when contents of messages.json are available, do this:
+	    // when contents of messages.json are available, do this:
 	    messageRequest.addEventListener("load", function() {
 	      var messagesData = JSON.parse(this.responseText);
-	      var messages = messagesData.messages;
-	      console.log("messages", messages);
+	      messages = messagesData.messages;
+	      return messages;
 	    });
 	  }
 	}
