fe1e0c8c5abaa26150d8b84b48aa6e6be3e7745d
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro-2.local>
2016-02-15 13:17:01 -0600

    finished getMessages and outputToDOM

---
+++ output.js
@@ -0,0 +1,40 @@
+var Chatty = function (output) {
+	var messageArray = [];
+
+	// is this function allowed?
+	output.addMessage = function(userInput) {
+		messageArray.push(userInput);
+		Chatty.outputToDOM(messageArray.length, userInput);
+	}
+
+	output.getMessages = function(arr) {
+		arr.forEach(function(msg) {
+			messageArray.push(msg.message);
+		});
+		console.log("array", messageArray);
+		// get messages into DOM -- not sure if this should go here
+		for (var i = 0; i < messageArray.length; i++) {
+			var id = i;
+			console.log("id", id);
+			var input = messageArray[i];
+			console.log("input", input);
+			Chatty.outputToDOM(id, input);
+		};
+		return messageArray;
+	}
+
+	output.outputToDOM = function(id, input) {
+		// remove disabled attribute on delete button once messages are on DOM
+		var messageDiv = "";
+		messageDiv += `<div id=${id}>`;
+		messageDiv += `<span>${input}</span>`;
+		messageDiv += `<button type="delete-single" class="delete-single" id=${id}>Delete Message</button>`;
+		messageDiv += `</div>`;
+		container.innerHTML += messageDiv;
+	}
+
+	// output.deleteMessage = function() {
+	// }
+
+	return output;
+}(Chatty);
